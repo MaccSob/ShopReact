42275d848f8165de9c6b512471835b2654deb0eb
MaccSob <71sobmaciej@gmail.com>
2024-12-09 23:52:00 +0100

    2

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Main.css'
+
+
+
+const Main = () => {
+
+    return (
+        <>
+       <h1>Polska gurom Shop</h1>
+     
+        </>
+    )
+}
+export default Main;
