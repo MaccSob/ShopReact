c7488d270bc0cceede5ba2167f39c1e954db9f3d
MaccSob <71sobmaciej@gmail.com>
2024-12-23 00:48:38 +0100

    changed p font in browse

--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -10,60 +10,74 @@ const Browse = () => {
                     <div className="img-wrap-square">
                 <img src="/src/assets/grape.jpg" alt=""/>
                 </div>
+                <p><button>More</button></p>
                     <h4>Grape Juice</h4>
                     <p>Freshly made juice from delicious moldovian grapes!</p>
-                    <p><button>More</button></p>
                     </div>
                     <div className="card">
                     <div className="img-wrap-square">
                 <img src="/src/assets/pineapple.jpg" alt="" />
                 </div>
+                <p><button>More</button></p>
                     <h4>Pineapple Juice</h4>
-                    <p>Recommended for males.</p>
-                    <p><button>More</button></p>
+                    <p>A men's favourite, for some reason.</p>
                     </div>
                     <div className="card">
                     <div className="img-wrap-square">
                 <img src="/src/assets/dragonfruit.jpg" alt="" />
                 </div>
+                <p><button>More</button></p>
                     <h4>Dragonfruit Juice</h4>
                     <p>Dovakhiin's favourite.</p>
-                    <p><button>More</button></p>
                     </div>
                     <div className="card">
                     <div className="img-wrap-square">
                 <img src="/src/assets/lemon.jpg" alt="" />
                 </div>
+                <p><button>More</button></p>
                     <h4>Lemon Juice</h4>
                     <p>Plenty of vitamin c right there!</p>
-                    <p><button>More</button></p>
                     </div>
                     <div className="card">
                     <div className="img-wrap-square">
                 <img src="/src/assets/orange.jpg" alt="" />
                 </div>
+                <p><button>More</button></p>
                     <h4>Orange Juice</h4>
                     <p>Don't drink after brushing your teeth!</p>
-                    <p><button>More</button></p>
                     </div>
                     <div className="card">
                     <div className="img-wrap-square">
                 <img src="/src/assets/carrot.jpg" alt="" />
                 </div>
+                <p><button>More</button></p>
                     <h4>Carrot Juice</h4>
                     <p>What's up doc?</p>
-                    <p><button>More</button></p>
                     </div>
                     <div className="card">
                     <div className="img-wrap-square">
                 <img src="/src/assets/banana.jpg" alt="" />
                 </div>
+                <p><button>More</button></p>
                     <h4>Banana Juice</h4>
                     <p>Great source of potassium!</p>
-                    <p><button>More</button></p>
                     </div>
-                <div className="">8</div>
-                <div className="">9</div>
+                    <div className="card">
+                    <div className="img-wrap-square">
+                <img src="/src/assets/greenapple.jpg" alt="" />
+                </div>
+                <p><button>More</button></p>
+                    <h4>Ripe Green Apple Juice</h4>
+                    <p>An apple a day keeps the doctor away, but what about a dozen of them?</p>
+                    </div>
+                    <div className="card">
+                    <div className="img-wrap-square">
+                <img src="/src/assets/melon.jpg" alt="" />
+                </div>
+                <p><button>More</button></p>
+                    <h4>Watermelon Juice</h4>
+                    <p>Wow is it sweet! No, really it is.</p>
+                    </div>
             </div>
         </>
     )
