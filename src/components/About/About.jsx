86dd013ed13a398a0f38533a05f94c737601aa90
MaccSob <71sobmaciej@gmail.com>
2024-12-24 00:18:41 +0100

    About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,17 @@ const About = () => {
         <>
             <h3>Who are we?</h3>
         
-        <section>S!</section>
+        <section>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut vestibulum orci, a tincidunt massa. Proin a orci interdum, consequat dui quis, mollis augue. Donec pharetra dui eget lectus mollis, sit amet sagittis massa euismod. Nullam quis nulla nec sem aliquet sagittis laoreet ac lectus. Sed ultrices tortor diam, sit amet consequat turpis pulvinar eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales neque lacinia, ultrices purus semper, maximus nibh. Integer vulputate faucibus eros quis euismod. Vivamus at suscipit sapien.</p>
+
+
+
+        <h3>Why us?</h3>
+
+        <p>Suspendisse massa felis, venenatis in volutpat non, semper sed lectus. Proin at libero in est commodo aliquam. Integer condimentum ipsum nec diam finibus fringilla. Ut lobortis a justo vitae cursus. Sed ultrices consectetur ex, aliquet tincidunt ligula lobortis a. Aenean consectetur malesuada mi gravida placerat. Maecenas vehicula lectus pretium leo facilisis, ac scelerisque nulla accumsan. Morbi cursus venenatis dui, at egestas neque auctor sit amet. Quisque rhoncus arcu id lobortis consequat.
+
+</p>    
+</section>
         </>
     )
 }
