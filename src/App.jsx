e498d1ff5954dbb7c88a0eddbec007d1283191ae
MaccSob <71sobmaciej@gmail.com>
2024-12-30 00:39:09 +0100

    Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-import Sidebar from './components/Sidebar/Sidebar.jsx'
 import Home from './components/Main/Home.jsx'
 import Header from './components/Header/Header.jsx'
 import About from './components/About/About.jsx';
 import Browse from './components/Browse/Browse.jsx';
+import Contact from './components/Contact/Contact.jsx'
+
 
 function App() {
  
@@ -17,6 +18,7 @@ function App() {
        <Route path="/" element={<Home />} />
          <Route path="/About" element={<About />} />
          <Route path="/Browse" element={<Browse />} />
+         <Route path="/Contact" element={<Contact />} />
        </Routes>
     </>
       </Router>
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Contact.css'
+
+
+
+const Contact = () => {
+
+    return (
+        <>
+       <h2>Need help? </h2>
+     
+        </>
+    )
+}
+export default Contact;
