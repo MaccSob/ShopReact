955962b047da9545842860444f08b96a1b97d320
MaccSob <71sobmaciej@gmail.com>
2025-01-03 00:31:41 +0100

    accordions

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,21 +1,46 @@
 import React from "react";
 import './Contact.css'
+import Accordion from './FAQ';
 
 
 
 const Contact = () => {
+    const accordionData = [
+        {
+          title: 'Where can i find the receipt?',
+          content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+          laborum cupiditate possimus labore, hic temporibus velit dicta earum
+          suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+          voluptatem.`
+        },
+        {
+          title: 'Section 2',
+          content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+          reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+          quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+          pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+          quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+          Repudiandae, mollitia id reprehenderit a ab odit!`
+        },
+        {
+          title: 'Section 3',
+          content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
+          quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
+          dolor ut sequi minus iste? Quas?`
+        }
+      ];
 
     return (
         <>
        <h2>Need help?  </h2>
        <h3>Choose a preferred a channel of communication and let us help you!</h3>
-
-        <section>
-            <h3>Frequently asked questions:</h3>
-            <li>Where can i find the receipt? </li>
-            <li></li>
-            <li></li>
-            <li></li>
+       <section>
+        <h3>Frequently asked questions:</h3>
+       <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
         </section>
      
         </>
