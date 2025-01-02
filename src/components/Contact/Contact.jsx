6a046b49de650a379ae8dbdd4c874234255c5638
MaccSob <71sobmaciej@gmail.com>
2025-01-02 23:55:24 +0100

    doing faq in contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,16 @@ const Contact = () => {
 
     return (
         <>
-       <h2>Need help? </h2>
+       <h2>Need help?  </h2>
+       <h3>Choose a preferred a channel of communication and let us help you!</h3>
+
+        <section>
+            <h3>Frequently asked questions:</h3>
+            <li>Where can i find the receipt? </li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </section>
      
         </>
     )
